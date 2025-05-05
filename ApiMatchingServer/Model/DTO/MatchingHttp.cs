645b6eedf0b95babd633b8357b58ac1d1c1cf89a
hhes0225 <hhes0225@gmail.com>
2025-05-06 03:58:11 +0900

    feat: 매칭 요청(최초 요청) 시 마지막 플레이 결과를 받도록 변경

--- ApiMatchingServer/Model/DTO/MatchingHttp.cs
+++ ApiMatchingServer/Model/DTO/MatchingHttp.cs
@@ -3,13 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ApiMatchingServer.Models.DTO;
 
+public enum GameResult
+{
+    Win = 0,
+    Lose = 1,
+    Draw = 2,
+    None = 3
+}
+
 public class MatchingRequest
 {
 	public string UserID { get; set; }=string.Empty;
 	public int TierScore { get; set; }=0;
+    public GameResult LastGameResult {  get; set; }=GameResult.None;
 }
 
-public class MatchResponse
+public class MatchingResponse
 {
     [Required]
     public ErrorCode Result { get; set; } = ErrorCode.None;
