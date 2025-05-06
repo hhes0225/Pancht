3216634a548c95db7e342eb3ad35f8dcdb696675
hhes0225 <hhes0225@gmail.com>
2025-05-07 04:46:54 +0900

    chore(config): change server port

--- ApiMatchingServer/Program.cs
+++ ApiMatchingServer/Program.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 using ApiMatchingServer;
+using ApiMatchingServer.Repository;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,15 +16,17 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 IConfiguration configuration = builder.Configuration;
 builder.Services.Configure<MatchingConfig>(configuration.GetSection(nameof(MatchingConfig)));
 
+//Repository DI
+builder.Services.AddSingleton<IMemoryDb, MemoryDb>();
 builder.Services.AddSingleton<IMatchWoker, MatchWoker>();
 
-
 builder.Services.AddControllers();
+builder.WebHost.UseUrls(configuration["ServerUrl"]);
+
 
 WebApplication app = builder.Build();
 
 app.MapControllers();
-
-app.Run();
+app.Run(configuration["ServerUrl"]);
 
 
